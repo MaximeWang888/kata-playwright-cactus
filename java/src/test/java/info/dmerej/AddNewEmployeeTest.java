cf9839c371f63ea79b27ba21303d153410e9c0d0
Maxime Wang <maxime.wang@efrei.net>
2025-02-24 10:24:18 +0100

    Add one test ResetManager, missing assert TODO

--- java/src/test/java/info/dmerej/AddNewEmployeeTest.java
+++ java/src/test/java/info/dmerej/AddNewEmployeeTest.java
@@ -1,13 +1,9 @@
 package info.dmerej;
 
-import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.junit.UsePlaywright;
-import com.microsoft.playwright.options.*;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -17,26 +13,9 @@ public class AddNewEmployeeTest extends Setup{
     @Test
     void AddNewEmployeeTest(Page page) {
         page.navigate("https://c.lsi2.hr.dmerej.info/");
-        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Reset database")).click();
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Proceed")).click();
-        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add new employee")).click();
-        page.getByPlaceholder("Name").click();
-        page.getByPlaceholder("Name").fill("Maxime");
-        page.getByPlaceholder("Name").press("Tab");
-        page.getByPlaceholder("Email").fill("maxime.wang@efrei.net");
-        page.getByPlaceholder("Email").press("Tab");
-        page.locator("#id_address_line1").press("ArrowDown");
-        page.locator("#id_address_line1").fill("13 rue de Chinatown");
-        page.getByPlaceholder("City").click();
-        page.getByPlaceholder("City").fill("Paris");
-        page.getByPlaceholder("City").press("Tab");
-        page.getByPlaceholder("Zip code").fill("75013");
-        page.getByPlaceholder("Hiring date").fill("2002-05-01");
-        page.getByPlaceholder("Job title").click();
-        page.getByPlaceholder("Job title").fill("D");
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
+        Helper.createUser(page, "Maxime", "maxime.wang@gmail.com", "13 rue du bronks", "Paris", "75013", "2002-05-01");
         assertThat(page.locator("tbody")).containsText("Maxime");
-        assertThat(page.locator("tbody")).containsText("maxime.wang@efrei.net");
+        assertThat(page.locator("tbody")).containsText("maxime.wang@gmail.com");
         assertThat(page.locator("tbody")).containsText("no");
     }
 }
---
+++ java/src/test/java/info/dmerej/ResetManagerTest.java
@@ -0,0 +1,22 @@
+package info.dmerej;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.Test;
+
+@UsePlaywright
+public class ResetManagerTest extends Setup {
+    @Test
+    void ResetManager(Page page) {
+        page.navigate("https://c.lsi2.hr.dmerej.info/");
+        Helper.createUser(page, "Maxime", "maxime.wang@gmail.com", "13 rue du bronks", "Paris", "75013", "2002-05-01");
+        page.navigate("https://c.lsi2.hr.dmerej.info/");
+
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Home")).click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List employees")).click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Edit")).first().click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Promote as manager")).click();
+        // Todo end with assert
+    }
+}
